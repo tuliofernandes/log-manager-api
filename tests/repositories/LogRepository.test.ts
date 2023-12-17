5b0ad3ebac5a4f35fbd7fd6b3f703ce38ac6ec17
Túlio Fernandes <tuliomf09@gmail.com>
2023-12-16 21:59:37 -0300

    test: should throw on a database error

--- tests/repositories/LogRepository.test.ts
+++ tests/repositories/LogRepository.test.ts
@@ -4,6 +4,8 @@ import { Log } from "@/models/Log";
 jest.mock("@/models/Log");
 
 describe("[Repository] Log", () => {
+  const sut = new LogRepository();
+
   const logFixture = {
     ip: "192.168.0.11",
     date: new Date(),
@@ -14,12 +16,22 @@ describe("[Repository] Log", () => {
   };
 
   it("should call mongoose create method with correct values", async () => {
-    const sut = new LogRepository();
-
     const createSpy = jest.spyOn(Log, "create");
 
     await sut.insert(logFixture);
 
     expect(createSpy).toHaveBeenCalledWith(logFixture);
+  });
+
+  it("should throw on a database error", async () => {
+    jest.spyOn(Log, "create").mockImplementationOnce(async () => {
+      throw new Error("mongo connection error");
+    });
+
+    const promise = sut.insert(logFixture);
+
+    await expect(promise).rejects.toThrow(
+      new Error("DatabaseError: mongo connection error")
+    );
   });
 });
