34072320e47bb2ac9e8cb2f4396359dcb2028570
Túlio Fernandes <tuliomf09@gmail.com>
2023-12-16 21:58:44 -0300

    feat: should throw on a database error

--- src/repositories/LogRepository.ts
+++ src/repositories/LogRepository.ts
@@ -2,7 +2,12 @@ import { Log as LogSchema, ILog } from "../models/Log";
 
 export class LogRepository {
   public async insert(log: ILog): Promise<ILog> {
-    const created = await LogSchema.create(log);
-    return created;
+    try {
+      const created = await LogSchema.create(log);
+      return created;
+    } catch (err) {
+      const error = err as Error;
+      throw new Error(`DatabaseError: ${error.message}`);
+    }
   }
 }
