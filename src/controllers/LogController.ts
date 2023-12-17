cb543f72e697ce6261d4c304676e3cc20d22aaa8
Túlio Fernandes <tuliomf09@gmail.com>
2023-12-17 20:14:36 -0300

    feat: ensure LogController is able to throw 404 if no file(s) are provided

---
+++ src/controllers/LogController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+
+interface IController {
+  handle(request: Request, response: Response): Promise<Response>;
+}
+
+export class UploadLogsController implements IController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { files } = request;
+
+      if (!files || files.length === 0) {
+        throw new Error("BadRequest: 'files' invalid or not provided");
+      }
+
+      return response.json({});
+    } catch (error) {
+      if ((error as Error).message.includes("BadRequest")) {
+        return response.status(400).json({
+          error: (error as Error).message,
+        });
+      }
+
+      return response.status(500).json({
+        error: "Internal Server Error",
+      });
+    }
+  }
+}
