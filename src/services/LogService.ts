6cb040af5277c6390e19df3500f901de51e4305f
Túlio Fernandes <tuliomf09@gmail.com>
2023-12-18 14:13:27 -0300

    refactor: wipe the ILogService interface

--- src/services/LogService.ts
+++ src/services/LogService.ts
@@ -3,15 +3,22 @@ import { LogRepository } from "../repositories/LogRepository";
 import { parseDatetime } from "../utils/parseDatetime";
 
 interface ILogService {
-  parseLogs(data: string): Promise<ILog[]>;
-  saveLogs(logs: ILog[]): Promise<void>;
   parseAndSaveLogs(data: string): Promise<void>;
 }
 
 export class LogService implements ILogService {
-  constructor(private readonly logRepository: LogRepository) {} // Use camelCase for variable names
+  constructor(private readonly logRepository: LogRepository) {}
 
-  async parseLogs(data: string): Promise<ILog[]> {
+  public async parseAndSaveLogs(data: string | Buffer): Promise<void> {
+    const logs = await this.parseLogs(data.toString());
+    await this.saveLogs(logs);
+  }
+
+  private async saveLogs(logs: ILog[]): Promise<void> {
+    await this.logRepository.createMany(logs);
+  }
+
+  private async parseLogs(data: string): Promise<ILog[]> {
     const lines: string[] = data.split("\n");
     const logs: ILog[] = [];
 
@@ -36,14 +43,5 @@ export class LogService implements ILogService {
     }
 
     return logs;
-  }
-
-  async saveLogs(logs: ILog[]): Promise<void> {
-    await this.logRepository.createMany(logs);
-  }
-
-  async parseAndSaveLogs(data: string | Buffer): Promise<void> {
-    const logs = await this.parseLogs(data.toString());
-    await this.saveLogs(logs);
   }
 }
