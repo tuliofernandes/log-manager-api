8343e50571e33022710065546f3a1db199c4228d
Túlio Fernandes <tuliomf09@gmail.com>
2023-12-18 15:50:33 -0300

    feat: ensure LogRepository findMany is able to call mongoose find with the correct values

--- src/repositories/LogRepository.ts
+++ src/repositories/LogRepository.ts
@@ -1,3 +1,5 @@
+import { FilterQuery } from "mongoose";
+
 import { DatabaseError } from "../errors/DatabaseError";
 import { Log as LogSchema, ILog } from "../models/Log";
 
@@ -5,6 +7,23 @@ export class LogRepository {
   public async createMany(logs: ILog[]): Promise<void> {
     try {
       await LogSchema.insertMany(logs);
+    } catch (error) {
+      throw new DatabaseError((error as Error).message);
+    }
+  }
+
+  public async findMany(
+    startDate: Date,
+    endDate: Date,
+    messagePattern: string
+  ): Promise<ILog[]> {
+    try {
+      const filter: FilterQuery<ILog> = {
+        datetime: { $gte: startDate, $lte: endDate },
+        description: { $regex: new RegExp(messagePattern, "i") },
+      };
+
+      return await LogSchema.find(filter);
     } catch (error) {
       throw new DatabaseError((error as Error).message);
     }
