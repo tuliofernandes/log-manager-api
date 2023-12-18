16861ce7244738e5f49257363dd2d976114dde2c
Túlio Fernandes <tuliomf09@gmail.com>
2023-12-17 21:42:44 -0300

    feat: ensure LogController is able to call LogService.parseAndSaveLogs with the files contents

--- src/controllers/LogController.ts
+++ src/controllers/LogController.ts
@@ -1,4 +1,6 @@
 import { BadRequestError } from "@/errors/BadRequestError";
+import { LogRepository } from "@/repositories/LogRepository";
+import { LogService } from "@/services/LogService";
 import { Request, Response } from "express";
 
 interface IController {
@@ -8,11 +10,20 @@ interface IController {
 export class UploadLogsController implements IController {
   public async handle(request: Request, response: Response): Promise<Response> {
     try {
-      const { files } = request;
+      const files = request.files;
 
       if (!files || files.length === 0) {
         throw new BadRequestError("'files' invalid or not provided");
       }
+
+      // TODO: Check if files are valid via mimetype
+
+      console.log((files as Express.Multer.File[])[0].buffer.toString());
+
+      const logService = new LogService(new LogRepository());
+      await logService.parseAndSaveLogs(
+        (files as Express.Multer.File[])[0].buffer
+      );
 
       return response.status(201).send();
     } catch (error) {
