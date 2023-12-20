61a88570e2e812ade10013dab97749fa57ae26bd
Túlio Fernandes <tuliomf09@gmail.com>
2023-12-20 13:43:49 -0300

    feat: ensure errorHandler is able to return 400 and the error message if the error message includes 'BadRequestError'

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+
+function errorHandler(
+  error: Error,
+  request: Request,
+  response: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+) {
+  if (error.message.includes("BadRequestError")) {
+    return response.status(400).json({
+      error: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    error: "Internal Server Error",
+  });
+}
+
+export default errorHandler;
